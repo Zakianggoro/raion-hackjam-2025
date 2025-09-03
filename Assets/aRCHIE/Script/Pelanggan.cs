05a181b77475846eb0183d5555b79d4898d5ee56
Archie <57532087+Rockblank@users.noreply.github.com>
2025-09-04 00:45:59 +0700

    Pelanggan
    Setting up Pelanggan Basic behaviourss

---
+++ Assets/aRCHIE/Script/Pelanggan.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Timers;
+using UnityEngine;
+
+public class Pelanggan : MonoBehaviour
+{
+    [Header("Sprites Settings")]
+    [SerializeField] SpriteRenderer charS;
+    [SerializeField] SpriteRenderer dialogS;
+    [SerializeField] SpriteRenderer foodS;
+    [SerializeField] Sprite happyChar;
+    [SerializeField] Sprite madChar;
+    [SerializeField] Sprite foodsSprite;
+
+    [Header("Waiting time")]
+    [SerializeField] float waitTime = 15.0f;
+
+    [Header("Location Setting")]
+    [SerializeField] Vector2 startPos;
+    [SerializeField] Vector2 targetPos;
+
+    [Header("Move settings")]
+    [SerializeField] float moveSpeed;
+
+    [Header("Boolean Settings")]
+    bool isOnFinalPosition = false;
+    bool isLeaving = false;
+    bool isServed = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+        dialogS.enabled = false;
+        foodS.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isOnFinalPosition && !isLeaving)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, targetPos) < 0.01f)
+            {
+                isOnFinalPosition = true;
+                dialogS.enabled = true;
+                foodS.enabled = true;
+                StartCoroutine(timerCoroutine());
+            }
+        }
+        else if (isLeaving || isServed)
+        {
+            if (isLeaving && !isServed)
+            {
+                charS.sprite = madChar;
+                dialogS.enabled = false;
+                foodS.enabled = false;
+            }
+            else if (isServed)
+            {
+                charS.sprite = happyChar;
+                dialogS.enabled = false;
+                foodS.enabled = false;
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, startPos, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, startPos) < 0.01f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    IEnumerator timerCoroutine()
+    {
+        float timeSpend = 0f;
+        while (timeSpend < waitTime)
+        {
+            if (isServed) { yield break; }
+            timeSpend += Time.deltaTime;
+            yield return null;
+        }
+
+        isLeaving = true;
+    }
+
+    public void MenuServed()
+    {
+        isServed = true;
+    }
+}
